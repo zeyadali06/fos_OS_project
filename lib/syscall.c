34ffe229b340b0d6fa4a300b8751d29d2c2a2c65
GABASKI <98767071+GABASKI@users.noreply.github.com>
2023-12-04 21:03:58 +0200

    modfied sys call

--- lib/syscall.c
+++ lib/syscall.c
@@ -349,6 +349,7 @@ uint32 sys_get_hard_limit()
 {
 	return syscall(SYS_get_hard_limit, 0, 0, 0, 0, 0);
 }
-void sys_env_set_nice(struct Env *e ,int nice ){
-	syscall(SYS_set_nice, e->nice , nice, 0 , 0 , 0  );
+void sys_env_set_nice(int nice ){
+	syscall(SYS_set_nice, nice , 0, 0 , 0 , 0  );
+	
 }
