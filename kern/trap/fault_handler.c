024449a157c4a090ad5434467bc08df3aa766bc2
zeyad-1 <z01551153743@gmail.com>
2023-12-06 01:57:06 +0200

    hanaka

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -260,7 +260,7 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 	{
 		fault_va &= 0xFFFFF000;
 		// cprintf("--------------------------------------------------\n");
-
+		// cprintf("va: %x\n", fault_va);
 		uint32 activeListSize = LIST_SIZE(&(curenv->ActiveList));
 		uint32 secondListSize = LIST_SIZE(&(curenv->SecondList));
 
@@ -277,11 +277,11 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 
 			if (activeListSize < curenv->ActiveListSize)
 			{
-				// cprintf("active not full\n");
+				cprintf("active not full\n");
 				struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
 				LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
-				// env_page_ws_print(curenv);
-				// cprintf("--------------------------------------------------\n");
+				env_page_ws_print(curenv);
+				cprintf("--------------------------------------------------\n");
 			}
 			else
 			{
@@ -291,7 +291,7 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 					if ((uint32)(currele->virtual_address & 0xFFFFF000) == (uint32)(fault_va & 0xFFFFF000))
 					{
 
-						// cprintf("insertion\n");
+						cprintf("insertion\n");
 						struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
 
 						struct WorkingSetElement *firstListLastEle1 = LIST_LAST(&(curenv->ActiveList));
@@ -304,14 +304,14 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 						pt_set_page_permissions(curenv->env_page_directory, ele->virtual_address, PERM_PRESENT, 0);
 						LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
 
-						// env_page_ws_print(curenv);
-						// cprintf("--------------------------------------------------\n");
+						env_page_ws_print(curenv);
+						cprintf("--------------------------------------------------\n");
 
 						return;
 					}
 				}
 
-				// cprintf("insertion faild\n");
+				cprintf("insertion faild\n");
 				struct WorkingSetElement *firstListLastEle1 = LIST_LAST(&(curenv->ActiveList));
 
 				LIST_REMOVE(&(curenv->ActiveList), firstListLastEle1);
@@ -322,8 +322,8 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 				pt_set_page_permissions(curenv->env_page_directory, firstListLastEle1->virtual_address, PERM_PRESENT, 0);
 				LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
 
-				// env_page_ws_print(curenv);
-				// cprintf("--------------------------------------------------\n");
+				env_page_ws_print(curenv);
+				cprintf("--------------------------------------------------\n");
 				return;
 			}
 		}
@@ -332,6 +332,7 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 			// TODO: [PROJECT'23.MS3 - #2] [1] PAGE FAULT HANDLER - LRU Replacement
 			//  Write your code here, remove the panic and write your code
 
+			cprintf("------------------------------------------------------------\n");
 			cprintf("Replacment va: %x\n", fault_va);
 
 			// struct WorkingSetElement *currele;
@@ -351,43 +352,43 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 			// }
 
 			// cprintf("Replacment\n");
+			struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
 			struct FrameInfo *frame_info_ptr;
 			if (allocate_frame(&frame_info_ptr) == 0)
 			{
-				map_frame(curenv->env_page_directory, frame_info_ptr, fault_va, PERM_USER | PERM_WRITEABLE);
+				map_frame(curenv->env_page_directory, frame_info_ptr, fault_va, PERM_MARKED | PERM_USER | PERM_WRITEABLE);
 				frame_info_ptr->va = fault_va;
-				pf_read_env_page(curenv, (void *)fault_va);
+				frame_info_ptr->element = ele;
+				int ret = pf_read_env_page(curenv, (void *)fault_va);
+				// pt_set_page_permissions(curenv->env_page_directory, fault_va,PERM_PRESENT , 0);
 			}
 
-			struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
+			struct WorkingSetElement *currele;
+			LIST_FOREACH(currele, &(curenv->SecondList))
+			{
+				if ((uint32)(currele->virtual_address & 0xFFFFF000) == (uint32)(fault_va & 0xFFFFF000))
+				{
 
-			// struct WorkingSetElement *currele;
-			// LIST_FOREACH(currele, &(curenv->SecondList))
-			// {
-			// 	if ((uint32)(currele->virtual_address & 0xFFFFF000) == (uint32)(fault_va & 0xFFFFF000))
-			// 	{
-			//
-			// 		// cprintf("insertion\n");
-			// 		struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
-			//
-			// 		struct WorkingSetElement *firstListLastEle1 = LIST_LAST(&(curenv->ActiveList));
-			// 		LIST_REMOVE(&(curenv->ActiveList), firstListLastEle1);
-			// 		pt_set_page_permissions(curenv->env_page_directory, firstListLastEle1->virtual_address, 0, PERM_PRESENT);
-			// 		LIST_INSERT_HEAD(&(curenv->SecondList), firstListLastEle1);
-			//
-			// 		struct WorkingSetElement *currele1 = currele;
-			// 		LIST_REMOVE(&(curenv->SecondList), currele1);
-			// 		pt_set_page_permissions(curenv->env_page_directory, ele->virtual_address, PERM_PRESENT, 0);
-			// 		LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
-			//
-			// 		// env_page_ws_print(curenv);
-			// 		// cprintf("--------------------------------------------------\n");
-			//
-			// 		cprintf("Finish1\n");
-			//
-			// 		return;
-			// 	}
-			// }
+					// cprintf("insertion\n");
+					// struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
+
+					struct WorkingSetElement *firstListLastEle1 = LIST_LAST(&(curenv->ActiveList));
+					LIST_REMOVE(&(curenv->ActiveList), firstListLastEle1);
+					pt_set_page_permissions(curenv->env_page_directory, firstListLastEle1->virtual_address, 0, PERM_PRESENT);
+					LIST_INSERT_HEAD(&(curenv->SecondList), firstListLastEle1);
+
+					struct WorkingSetElement *currele1 = currele;
+					LIST_REMOVE(&(curenv->SecondList), currele1);
+					pt_set_page_permissions(curenv->env_page_directory, ele->virtual_address, PERM_PRESENT, 0);
+					LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
+
+					cprintf("Finish loop\n");
+					env_page_ws_print(curenv);
+					cprintf("--------------------------------------------------\n");
+
+					return;
+				}
+			}
 
 			struct WorkingSetElement *secondListLastEle = LIST_LAST(&(curenv->SecondList));
 			int perms = pt_get_page_permissions(curenv->env_page_directory, secondListLastEle->virtual_address);
@@ -406,10 +407,12 @@ void page_fault_handler(struct Env *curenv, uint32 fault_va)
 			pt_set_page_permissions(curenv->env_page_directory, firstListLastEle1->virtual_address, 0, PERM_PRESENT);
 			LIST_INSERT_HEAD(&(curenv->SecondList), firstListLastEle1);
 
-			pt_set_page_permissions(curenv->env_page_directory, ele->virtual_address, PERM_PRESENT, 0);
+			// pt_set_page_permissions(curenv->env_page_directory, ele->virtual_address, PERM_PRESENT, 0);
 			LIST_INSERT_HEAD(&(curenv->ActiveList), ele);
 
 			cprintf("Finish\n");
+			env_page_ws_print(curenv);
+			cprintf("------------------------------------------------------------\n");
 
 			// struct WorkingSetElement *ele = env_page_ws_list_create_element(curenv, fault_va);
 			// pt_set_page_permissions(curenv->env_page_directory, firstListLastEle1->virtual_address, PERM_PRESENT, 0);
@@ -447,3 +450,23 @@ void __page_fault_handler_with_buffering(struct Env *curenv, uint32 fault_va)
 // 7:           800000
 // 8:           205000
 // 9:           204000
+//
+//
+//
+//
+//
+// ActiveList: 5
+// ============
+// 0:           803000
+// 1:           801000
+// 2:           800000
+// 3:           eebfd000
+// 4:           203000
+//
+// SecondList: 5
+// ============
+// 5:           202000
+// 6:           201000
+// 7:           200000
+// 8:           802000
+// 9:           205000
